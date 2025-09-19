475d58656e6372f76e71eff5ea007e816be9a3ac
Clara de Borba Gutierrez Benito <clara.benito@sou.inteli.edu.br>
2025-09-19 08:09:25 -0300

    Refactor code structure for improved readability and maintainability

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -1,23 +1,38 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-from scipy.stats import linregress
+import pandas as pd #Importa a biblioteca ppandas 
+import matplotlib.pyplot as plt 
+from scipy.stats import linregress 
 
 def draw_plot():
-    # Read data from file
+    # Le os dados do arquivo CSV que contem informaçoes 
+    df = pd.read_csv("epa-sea-level.csv")
 
 
-    # Create scatter plot
+    # Cria um gráfico de dispersao 
+    plt.scatter(df["Year"], df["CSIRO Adjusted Sea Level"])
 
 
-    # Create first line of best fit
+    # Caucula os parametros da regressao linear
+    slope, intercept, _, _, _ = linregress(df["Year"], df["CSIRO Adjusted Sea Level"])
+    anos_todos = pd.Series(range(1880, 2051))
+    previsao_todos = intercept + slope * anos_todos
+    plt.plot(anos_todos, previsao_todos, color="red")
 
 
-    # Create second line of best fit
+    # Filtra o DataFrame para pegar apenas os registros de 2000 ate o resto.
+    df_2000 = df[df["Year"] >= 2000]
+    slope2, intercept2, _, _, _ = linregress(df_2000["Year"], df_2000["CSIRO Adjusted Sea Level"])
+    anos_2000 = pd.Series(range(2000, 2051))
+    previsao_2000 = intercept2 + slope2 * anos_2000
+    plt.plot(anos_2000, previsao_2000, color="green")
 
 
-    # Add labels and title
+    # Adiciona o título ao gráfico para melhor entendimento.
+    plt.xlabel("Year")
+    plt.ylabel("Sea Level (inches)")
+    plt.title("Rise in Sea Level")
+
 
     
-    # Save plot and return data for testing (DO NOT MODIFY)
-    plt.savefig('sea_level_plot.png')
+    # Salva o gráfico gerado como um arquivo PNG.
+    plt.savefig("sea_level_plot.png")
     return plt.gca()
